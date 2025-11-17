a53074d2feb05dd7cfab09aba2fb68e1c68aa446
samedyhunx <samedy.hunx@gmail.com>
2025-11-17 22:23:51 +0700

    main: prisma setup

---
+++ prisma.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: env(
+      "DATABASE_URL"
+      // "postgresql://postgres:randompassword@localhost:5433/blogs",
+    ),
+  },
+});
